bcb1f5d53f01385f14768b87eac20a23bcf4bc9d
unknown-user <unknown-user@fe80::a55f:ebb4:7fde:6f54%29>
2014-04-04 15:31:04 +0200

    new

--- Assets/Scripts/huds/GUIOptimizer.js
+++ Assets/Scripts/huds/GUIOptimizer.js
@@ -5,12 +5,40 @@ class GUIOptimizer extends MonoBehaviour
 	public var sarge : SargeManager;
 	public var achievements : AchievmentScreen;
 	public static var score : int = 0;
+	public static var dead : boolean = false;
+	public static var killed : boolean = false;
+	public var timer : Object;
+	private var startTimer : boolean = false;
 
 	function OnGUI()
 	{ 
 		var evt : Event = Event.current;
 		GUI.Box(Rect(Screen.width-300,0,150,30),"Score: "+score);
-		
+		if(killed){
+			if(!startTimer){
+				timer = System.DateTime.Now;
+				startTimer = true;
+			}
+		   GUI.Label(Rect(0,0,Screen.width,Screen.height),"You were killed!");
+		System.TimeSpan ts = new System.TimeSpan(System.DateTime.Now.Ticks - timer.Ticks);
+		   if(ts.Seconds > 5){
+			killed = false;
+			startTimer = false;
+			}
+		}
+		if(dead){
+		if(!startTimer){
+				timer = System.DateTime.Now;
+				startTimer = true;
+			}
+		   GUI.Label(Rect(0,0,Screen.width,Screen.height),"You were killed!");
+		System.TimeSpan ts = new System.TimeSpan(System.DateTime.Now.Ticks - timer.Ticks);
+		   if(ts.Seconds > 5){
+			dead = false;
+			startTimer = false;
+			}
+			
+		}
 		 
 		if(mainMenu != null) mainMenu.DrawGUI(evt);
  
